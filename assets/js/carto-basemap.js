a18bc696e51f42c3d43b85db6f20f0b35085cfe2
TobyB2 <toby.bridgeman@foe.co.uk>
2026-09-02 11:13:59 +0100

    correcting the code in thejs file for basemaps

--- assets/js/carto-basemap.js
+++ assets/js/carto-basemap.js
@@ -2,15 +2,30 @@
 
 const cartoKey = 'cb1_2s1u_1_8f25e8f342fa438d46d69300';
   
-  L.tileLayer(
+const cartoAttribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
+  '&copy; <a href="https://carto.com/attributions">CARTO</a>';
+
+function addCartoLightNoLabels(map, options = {}) {
+  return L.tileLayer(
     `https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png?key=${cartoKey}`,
     {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors ' +
-        '&copy; <a href="https://carto.com/attributions">CARTO</a>',
+      attribution: cartoAttribution,
       subdomains: 'abcd',
-      maxZoom: 18
+      maxZoom: 18,
+      ...options
     }
   ).addTo(map);
-  
+}
+
+function createCartoLightOnlyLabels(options = {}) {
+  return L.tileLayer(
+    `https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png?key=${cartoKey}`,
+    {
+      attribution: cartoAttribution,
+      subdomains: 'abcd',
+      maxZoom: 18,
+      ...options
+    }
+  );
 }
